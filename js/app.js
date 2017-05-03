b45b2c755eb9eba83846bcfa8eaaabda013e8ee4
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-05-03 16:54:28 -0500

    creando las funciones para mostrar el modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+ var boxWork=document.getElementsByClassName("imgProducto");
+
+ for (var i=0;i<boxWork.length;i++){
+   boxWork[i].addEventListener("click",function(){
+     abrir(this)
+   });
+ }
+
+ function abrir(imagenProducto){
+     var elemento=imagenProducto.src;
+     var imagen=crearModalImagen(elemento);
+     mostrarModal(imagen);
+ }
+
+ function crearModalImagen(imgsrc){
+   var fondoModal=document.getElementById("fondo");
+   var modal=document.getElementById("modal");
+   var imagenModal=document.getElementById("_imagen");
+   imagenModal.src=imgsrc;
+   imagenModal.style.width="90%";
+
+   fondo.className="ClaseImageProduct";
+   modal.className="modal";
+
+   var botonCerrar=document.getElementById("botonCerrar");
+   botonCerrar.innerText="X";
+   botonCerrar.className="botonCerrarModal";
+   botonCerrar.addEventListener("click",function(){
+   fondo.style.visibility="hidden";
+   });
+   return fondo;
+ }
+
+ function mostrarModal(modal){
+   modal.style.visibility="visible";
+ }
